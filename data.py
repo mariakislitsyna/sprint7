679d8376301202ceb946fa25c9cbe96ddcde1abe
Maria K <mkislitcyna@rbc.ru>
2025-04-13 11:18:45 +0200

    sprint7

---
+++ data.py
@@ -0,0 +1,33 @@
+class Users:
+    data_current = {
+        "login": "ninjafdfdf666",
+        "password": "1234666"
+    }
+    data_negative = {
+        "login": "test181920",
+        "password": "19874634"
+    }
+    data_without_login = {
+        "login": "",
+        "password": "19874634"
+    }
+    data_without_password = {
+        "login": "test181920",
+        "password": ""
+    }
+
+
+class Orders:
+    data_order = {
+        "firstName": "Maria",
+        "lastName": "Kislitsyna",
+        "address": "Moscow, street 12.",
+        "metroStation": 4,
+        "phone": "+7 999 999 99 99",
+        "rentTime": 5,
+        "deliveryDate": "2025-04-25",
+        "comment": "Comment test",
+        "color": [
+            "BLACK"
+        ]
+    }
